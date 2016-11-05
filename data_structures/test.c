481e076182fabf9c6d9db49053e408460d087433
simonppg <simonppg@gmail.com>
2016-11-04 23:43:58 -0600

    test(enqueue/dequeue): Testing queue to save and release items

--- data_structures/test.c
+++ data_structures/test.c
@@ -12,15 +12,69 @@ int clean_suite1(void)
     return 0;
 }
 
-static void test_init_queue(void)
+static void __queue_test(struct Queue *queue)
 {
-    struct Queue *queue;
-    queue = init_queue();
     CU_ASSERT_PTR_NOT_NULL_FATAL(queue);
     CU_ASSERT_PTR_NULL_FATAL(queue->Head);
     CU_ASSERT_PTR_NULL_FATAL(queue->Tail);
     CU_ASSERT_EQUAL_FATAL(queue->size, 0);
+}
+
+// Test the correctly initialization of the queue
+static void test_init_queue(void)
+{
+    struct Queue *queue;
+    queue = init_queue();
+    __queue_test(queue);
     free_queue(queue);
+}
+
+/*
+ * Test that the enqueue/dequeue save and release the items and 
+ * modify the size of the queue
+ */
+static void test_enqueue_dequeue()
+{
+    struct Queue *q;
+    q = init_queue();
+    __queue_test(q);
+    struct Item *i;
+
+    // Enqueue
+    enqueue(q, 1);
+    CU_ASSERT_EQUAL_FATAL(q->size, 1);
+
+    enqueue(q, 2);
+    CU_ASSERT_EQUAL_FATAL(q->size, 2);
+
+    enqueue(q, 3);
+    CU_ASSERT_EQUAL_FATAL(q->size, 3);
+
+    enqueue(q, 4);
+    CU_ASSERT_EQUAL_FATAL(q->size, 4);
+
+    // Dequeue
+    i = dequeue(q);
+    CU_ASSERT_EQUAL_FATAL(i->value, 1);
+    CU_ASSERT_EQUAL_FATAL(q->size, 3);
+    free(i);
+
+    i = dequeue(q);
+    CU_ASSERT_EQUAL_FATAL(i->value, 2);
+    CU_ASSERT_EQUAL_FATAL(q->size, 2);
+    free(i);
+
+    i = dequeue(q);
+    CU_ASSERT_EQUAL_FATAL(i->value, 3);
+    CU_ASSERT_EQUAL_FATAL(q->size, 1);
+    free(i);
+
+    i = dequeue(q);
+    CU_ASSERT_EQUAL_FATAL(i->value, 4);
+    CU_ASSERT_EQUAL_FATAL(q->size, 0);
+    free(i);
+
+    free_queue(q);
 }
 
 int main()
@@ -45,8 +99,12 @@ int main()
     /*
      * add the tests to the suite
      */
-    if (NULL ==
-        CU_add_test(pSuite, "test of init_queue()", test_init_queue)) {
+    if ((NULL ==
+         CU_add_test(pSuite, "test of init_queue()", test_init_queue))
+        || (NULL ==
+            CU_add_test(pSuite, "test of enqueue() and dequeue()",
+                        test_enqueue_dequeue))
+        ) {
         CU_cleanup_registry();
         return CU_get_error();
     }
