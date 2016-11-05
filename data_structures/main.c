69c6fd0dbffbcde3482c54932551e7300a894668
simonppg <simonppg@gmail.com>
2016-11-04 22:41:56 -0600

    chore(Makefile): some changes to indent and test memory leaks using valgrind

--- data_structures/main.c
+++ data_structures/main.c
@@ -4,22 +4,22 @@
 
 int main()
 {
-	struct Queue *q;
-	q = init_queue();
-	enqueue(q, 5);
-	enqueue(q, 4);
-	enqueue(q, 6);
-	struct Item *i;
-	i = dequeue(q);
-	printf("Data: %d\n", i->value);
-	free(i);
-	i = dequeue(q);
-	printf("Data: %d\n", i->value);
-	free(i);
-	i = dequeue(q);
-	printf("Data: %d\n", i->value);
-	free(i);
-	free_queue(q);
+    struct Queue *q;
+    q = init_queue();
+    enqueue(q, 5);
+    enqueue(q, 4);
+    enqueue(q, 6);
+    struct Item *i;
+    i = dequeue(q);
+    printf("Data: %d\n", i->value);
+    free(i);
+    i = dequeue(q);
+    printf("Data: %d\n", i->value);
+    free(i);
+    i = dequeue(q);
+    printf("Data: %d\n", i->value);
+    free(i);
+    free_queue(q);
 
-	return 0;
+    return 0;
 }
--- data_structures/test.c
+++ data_structures/test.c
@@ -4,48 +4,58 @@
 
 int init_suite1(void)
 {
-	return 0;
+    return 0;
 }
 
 int clean_suite1(void)
 {
-	return 0;
+    return 0;
 }
 
-void test_init_queue(void){
-	struct Queue *queue;
-	queue = init_queue();
-	CU_ASSERT_PTR_NOT_NULL(queue);
-	CU_ASSERT_PTR_NULL(queue->Head);
-	CU_ASSERT_PTR_NULL(queue->Tail);
-	CU_ASSERT_EQUAL(queue->size, 0);
+static void test_init_queue(void)
+{
+    struct Queue *queue;
+    queue = init_queue();
+    CU_ASSERT_PTR_NOT_NULL_FATAL(queue);
+    CU_ASSERT_PTR_NULL_FATAL(queue->Head);
+    CU_ASSERT_PTR_NULL_FATAL(queue->Tail);
+    CU_ASSERT_EQUAL_FATAL(queue->size, 0);
+    free_queue(queue);
 }
 
 int main()
 {
-	CU_pSuite pSuite = NULL;
+    CU_pSuite pSuite = NULL;
 
-	/* initialize the CUnit test registry */
-	if (CUE_SUCCESS != CU_initialize_registry())
-		return CU_get_error();
+    /*
+     * initialize the CUnit test registry
+     */
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
 
-	/* add a suite to the registry */
-	pSuite = CU_add_suite("Suite_1", init_suite1, clean_suite1);
-	if (NULL == pSuite) {
-		CU_cleanup_registry();
-		return CU_get_error();
-	}
+    /*
+     * add a suite to the registry
+     */
+    pSuite = CU_add_suite("Suite_1", init_suite1, clean_suite1);
+    if (NULL == pSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
 
-	/* add the tests to the suite */
-	if (NULL == CU_add_test(pSuite, "test of init_queue()", test_init_queue))
-	{
-		CU_cleanup_registry();
-		return CU_get_error();
-	}
+    /*
+     * add the tests to the suite
+     */
+    if (NULL ==
+        CU_add_test(pSuite, "test of init_queue()", test_init_queue)) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
 
-	/* Run all tests using the CUnit Basic interface */
-	CU_basic_set_mode(CU_BRM_VERBOSE);
-	CU_basic_run_tests();
-	CU_cleanup_registry();
-	return CU_get_error();
+    /*
+     * Run all tests using the CUnit Basic interface
+     */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
 }
